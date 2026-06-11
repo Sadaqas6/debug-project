4154a2f809b160f54d7760eb9a01d8aca863913e
sadaqasalaam <sadaqasalaam6@gmail.com>
2026-06-11 15:57:45 -0400

    fixed bug #5

--- src/main/java/nl/pluralsight/stagepass/service/BookingService.java
+++ src/main/java/nl/pluralsight/stagepass/service/BookingService.java
@@ -33,7 +33,7 @@ public class BookingService {
     }
 
     public List<Booking> getBookingsByConcert(Long concertId) {
-        return bookingRepository.findByConcertId(concertId);
+        return bookingRepository.findByConcertId( concertId);
     }
 
     @Transactional
